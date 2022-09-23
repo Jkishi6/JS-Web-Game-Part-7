79d9a85947597292388f7c38daa4c243fb775a67
Jkishi6 <jonathankishi6@gmail.com>
2022-09-22 21:05:52 -0700

    fixed parameter error

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -31,7 +31,7 @@ function newNonPlayableCharacter(x, y) {
         stop()
     }
 
-    async function walkNorth() {
+    async function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
         
@@ -39,7 +39,7 @@ function newNonPlayableCharacter(x, y) {
         stop()
     }
 
-    async function walkWest() {
+    async function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
 
@@ -47,7 +47,7 @@ function newNonPlayableCharacter(x, y) {
         stop()
     }
 
-    async function walkSouth() {
+    async function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
 
